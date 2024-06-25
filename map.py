3b2c87adec5243b14dc01ecd3c82a8ee597f1f01
David Valentine <david.valentine@gmail.com>
2024-06-25 13:43:59 -0700

    add map

--- map.py
+++ map.py
@@ -1,36 +1,8 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-
-
-st.write("""
-# Resilient
-Mpox Counts 
-""")
-mpox = "https://oss.resilientservice.mooo.com/resilientdata/cdc/output/ndss_Mpox_zip.csv"
-df = pd.read_csv(mpox)
-#df = df[df['Reporting Area'] == 'NEW YORK']
-df_gby = df.groupby(['date','Reporting Area','geometry','lat','lon']).sum(["Cumulative YTD Current MMWR Year","Current week"]).reset_index()
-#df = pd.read_csv("my_data.csv")
-st.map(df)
-
-
-st.scatter_chart(df, y=["Cumulative YTD Current MMWR Year","Current week"],
-                 x="date", y_label="Reporting Area")
-
-fig = px.scatter(
-    df_gby,
-    x="date",
-    y=["Cumulative YTD Current MMWR Year","Current week"],
-    color="Reporting Area",
-    size="Current week",
-    hover_data=["Reporting Area"],
-)
-
-event = st.plotly_chart(fig, key="mpox", on_select="rerun")
-
-event.selection
-
+import states
+from datetime import timedelta
 
 st.write("""
 # ILI
@@ -41,10 +13,13 @@ rsv = "https://oss.resilientservice.mooo.com/resilientdata/cdc/resp_net/Rates_of
 rsv_df = pd.read_csv(rsv)
 rsv_df["Week Ending Date"]=pd.to_datetime(rsv_df["Week Ending Date"], utc=True,  format="ISO8601")
 rsv_df["Week Ending Date"]=rsv_df["Week Ending Date"].dt.date
-rsv_df = rsv_df[(rsv_df["Surveillance Network"] != "Combined") &  (rsv_df["Site"] == "California") ]
+
+
+rsv_df = rsv_df[(rsv_df["Surveillance Network"] != "Combined") &  (rsv_df["Site"] != "Overall") ]
+rsv_df["State"] = rsv_df["Site"].apply(lambda x: states.us_state_to_abbrev[x])
 
 rsv_gby = rsv_df.groupby(["Surveillance Network","Season","Site","Week Ending Date"], as_index=False).max()
-rvs_df2=rsv_gby [["Surveillance Network","Season","Week Ending Date","Weekly Rate", "Cumulative Rate", "Site"]]
+rvs_df2=rsv_gby [["Surveillance Network","Season","Week Ending Date","Weekly Rate", "Cumulative Rate", "Site", "State"]]
 
 col1, col2 = st.columns(2)
 
@@ -61,20 +36,34 @@ min = rvs_df2[col].min()
 date_col='Week Ending Date'
 first_year = rvs_df2[date_col].min()
 last_year = rvs_df2[date_col].max()
-year = st.slider('Select Week',first_year,last_year, key=date_col)
+week = st.slider('Select Week', first_year, last_year, key=date_col)
+week_plus6 = week + timedelta(weeks = 9)
+rsv_df_map = rvs_df2[(rvs_df2[date_col] >= week) & (rvs_df2[date_col] <= week_plus6) ]
 
-rsv_map = px.choropleth(rvs_df2[rvs_df2[date_col]==year],
-                    locations="Site",
-                 #   scope='US',
-                    color=col,
-                    hover_name="Surveillance Network",
-                    range_color=(min,max),
+# max = rsv_df_map[col].max()
+# min = rsv_df_map[col].min()
+
+rsv_map = px.choropleth( rsv_df_map,
+                        locations="State",
+                        locationmode='USA-states' ,
+                        scope="usa",
+                        color=col,
+                        hover_name="Surveillance Network",
+                        range_color=(min,max),
                         title="resp_net"
-                    )
+                        )
 #col1.plotly_chart(rsv_map)
 st.plotly_chart(rsv_map)
+
+st.write("""
+# ILI
+California 
+""")
+rsv_df2 = rsv_df[(rsv_df["Surveillance Network"] != "Combined") &  (rsv_df["Site"] == "California") ]
+rsv_gby2 = rsv_df2.groupby(["Surveillance Network","Season","Site","Week Ending Date"], as_index=False).max()
+rvs_df22=rsv_gby2 [["Surveillance Network","Season","Week Ending Date","Weekly Rate", "Cumulative Rate", "Site", "State"]]
 rsv_fig = px.scatter(
-    rvs_df2,
+    rvs_df22,
     x="Week Ending Date",
     y="Weekly Rate",
     color="Surveillance Network",
@@ -86,5 +75,4 @@ rsv_event = st.plotly_chart(rsv_fig, key="rsv", on_select="rerun")
 
 rsv_event.selection
 #col2.plotly_chart(rsv_fig)
-
 
